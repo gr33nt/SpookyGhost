793c63d369aa75cba7f618d3860711c8205980f0
Tristan <green.t@digipen.edu>
2019-12-29 03:28:13 -0800

    Moved the token into a JSon file. Updated properties to target proper version of .netcore

--- SpookyGhostBot/Program.cs
+++ SpookyGhostBot/Program.cs
@@ -1,108 +1,114 @@
 ﻿using System;
-using System.Collections;
 using Discord;
 using Discord.WebSocket;
 using System.Threading.Tasks;
 using Discord.Commands;
 using Microsoft.Extensions.DependencyInjection;
 using System.Reflection;
-using SpookyGhostBot.Modules;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Configuration.Json;
 
 namespace SpookyGhostBot
 {
-    public class Program
+  public class Program
+  {
+    static void Main(string[] args) => new Program().RunBotAsync().GetAwaiter().GetResult();
+
+    private readonly DiscordSocketClient _client;
+    private readonly CommandService _commands;
+    private readonly IConfiguration _config;
+
+    private IServiceProvider _services;
+    //public Currency currency = new Currency();
+
+    public Program()
     {
-        static void Main(string[] args) => new Program().RunBotAsync().GetAwaiter().GetResult();
+      _client = new DiscordSocketClient();
+      _commands = new CommandService();
+      _services = new ServiceCollection()
+          .AddSingleton(_client)
+          .AddSingleton(_commands)
+          .BuildServiceProvider();
 
-        private DiscordSocketClient _client;
+      //event subscriptions
+      _client.Log += Log;
 
-        private IServiceProvider _services;
-        private CommandService _commands = new CommandService();
-        //public Currency currency = new Currency();
-
-
-        public async Task RunBotAsync()
-        {
-            _client = new DiscordSocketClient();
-
-            
-
-            _services = new ServiceCollection()
-                .AddSingleton(_client)
-                .AddSingleton(_commands)
-                .BuildServiceProvider();
-
-            //event subscriptions
-            _client.Log += Log;
-
-            await RegisterCommandsAsync();
-
-            await _client.LoginAsync(TokenType.Bot, "MzY1MjU0MzEyMjQ0MjgxMzQ0.Xgh2Xg.O8tdKJsbkdsOP2eJbISG_3uLCjY");
-
-            await _client.StartAsync();
-
-            await Task.Delay(-1);
-        }
-
-        private Task Log(LogMessage message)
-        {
-            var cc = Console.ForegroundColor;
-            switch (message.Severity)
-            {
-                case LogSeverity.Critical:
-                case LogSeverity.Error:
-                    Console.ForegroundColor = ConsoleColor.Red;
-                    break;
-                case LogSeverity.Warning:
-                    Console.ForegroundColor = ConsoleColor.Yellow;
-                    break;
-                case LogSeverity.Info:
-                    Console.ForegroundColor = ConsoleColor.White;
-                    break;
-                case LogSeverity.Verbose:
-                case LogSeverity.Debug:
-                    Console.ForegroundColor = ConsoleColor.DarkGray;
-                    break;
-            }
-            Console.WriteLine($"{DateTime.Now,-19} [{message.Severity,8}] {message.Source}: {message.Message}");
-            Console.ForegroundColor = cc;
-
-            // If you get an error saying 'CompletedTask' doesn't exist,
-            // your project is targeting .NET 4.5.2 or lower. You'll need
-            // to adjust your project's target framework to 4.6 or higher
-            // (instructions for this are easily Googled).
-            // If you *need* to run on .NET 4.5 for compat/other reasons,
-            // the alternative is to 'return Task.Delay(0);' instead.
-            return Task.CompletedTask;
-        }
-
-        public async Task RegisterCommandsAsync()
-        {
-            _client.MessageReceived += HandleCommandAsync;
-
-            await _commands.AddModulesAsync(Assembly.GetEntryAssembly(), null);
-        }
-
-        private async Task HandleCommandAsync(SocketMessage arg)
-        {
-            var message = arg as SocketUserMessage;
-
-            if (message is null || message.Author.IsBot) return;
-
-            int argPos = 0;
-
-            if (message.HasStringPrefix("##", ref argPos) || message.HasMentionPrefix(_client.CurrentUser, ref argPos))
-            {
-                var context = new SocketCommandContext(_client, message);
-
-                var result = await _commands.ExecuteAsync(context, argPos, _services);
-
-                if (!result.IsSuccess)
-                {
-                    Console.WriteLine(result.ErrorReason);
-                    await message.Channel.SendMessageAsync(result.ErrorReason);
-                }
-            }
-        }
+      var _builder = new ConfigurationBuilder()
+                .SetBasePath(AppContext.BaseDirectory)
+                .AddJsonFile(path: "config.json");
+      _config = _builder.Build();
     }
+
+    public async Task RunBotAsync()
+    {
+      await RegisterCommandsAsync();
+
+      await _client.LoginAsync(TokenType.Bot, _config["Token"]);
+
+      await _client.StartAsync();
+
+      await Task.Delay(-1);
+    }
+
+    private Task Log(LogMessage message)
+    {
+      var cc = Console.ForegroundColor;
+      switch (message.Severity)
+      {
+        case LogSeverity.Critical:
+        case LogSeverity.Error:
+          Console.ForegroundColor = ConsoleColor.Red;
+          break;
+        case LogSeverity.Warning:
+          Console.ForegroundColor = ConsoleColor.Yellow;
+          break;
+        case LogSeverity.Info:
+          Console.ForegroundColor = ConsoleColor.White;
+          break;
+        case LogSeverity.Verbose:
+        case LogSeverity.Debug:
+          Console.ForegroundColor = ConsoleColor.DarkGray;
+          break;
+      }
+      Console.WriteLine($"{DateTime.Now,-19} [{message.Severity,8}] {message.Source}: {message.Message}");
+      Console.ForegroundColor = cc;
+
+      // If you get an error saying 'CompletedTask' doesn't exist,
+      // your project is targeting .NET 4.5.2 or lower. You'll need
+      // to adjust your project's target framework to 4.6 or higher
+      // (instructions for this are easily Googled).
+      // If you *need* to run on .NET 4.5 for compat/other reasons,
+      // the alternative is to 'return Task.Delay(0);' instead.
+      return Task.CompletedTask;
+    }
+
+    public async Task RegisterCommandsAsync()
+    {
+      _client.MessageReceived += HandleCommandAsync;
+
+      await _commands.AddModulesAsync(Assembly.GetEntryAssembly(), null);
+    }
+
+    private async Task HandleCommandAsync(SocketMessage arg)
+    {
+      var message = arg as SocketUserMessage;
+
+      if (message is null || message.Author.IsBot) return;
+
+      int argPos = 0;
+
+      if (message.HasStringPrefix("##", ref argPos) || message.HasMentionPrefix(_client.CurrentUser, ref argPos))
+      {
+        var context = new SocketCommandContext(_client, message);
+
+        var result = await _commands.ExecuteAsync(context, argPos, _services);
+
+        if (!result.IsSuccess)
+        {
+          Console.WriteLine(result.ErrorReason);
+          await message.Channel.SendMessageAsync(result.ErrorReason);
+        }
+      }
+    }
+  }
 }
